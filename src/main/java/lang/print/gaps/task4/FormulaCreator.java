32f6fc18ecd5a73e0ff6e443a20c3ed5cefdcf08
timtechuz <timur.galiaskarov@timtech.uz>
2022-08-10 12:40:50 +0500

    stage0-module3-task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+    	short a = 5;
+    	short b = 2;
+    	double c;
+    	
+    	c = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+    	
+    	System.out.println(c);
+    	
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+    	char c = 99;	// 'c'
+    	char h = 104;	// 'h'
+    	char a = 97;	// 'a'
+    	char r = 114;	// 'r'
+    	
+    	System.out.println(c);
+    	System.out.println(h);
+    	System.out.println(a);
+    	System.out.println(r);
     }
 }
